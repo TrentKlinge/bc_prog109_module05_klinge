389388c2b4ff440d105f8a07ba0d48e56f88cc42
TrentKlinge <82176438+TrentKlinge@users.noreply.github.com>
2021-05-09 23:25:21 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,21 +1,21 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var table = prompt("Enter a Value");            // Unit of table
+var operator = 'addition';                     // Type of calculation
+var i = 1;                                     // Set counter to 1
+var msg = 'Multiplication Table';              // Message
 
-if (operator === 'addition') {
+//if (operator === 'addition') {
   // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
+  //while (i < 11) {
+    //msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    //i++;
+  //}
+//} else {
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
+//}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
